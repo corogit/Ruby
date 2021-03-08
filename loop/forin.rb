e1f6b4f5199ae6e319b18790639213ca6032782d
corogit <66649016+corogit@users.noreply.github.com>
2021-03-08 22:42:33 +0900

    Add files via upload

--- loop/forin.rb
+++ loop/forin.rb
@@ -4,9 +4,31 @@ for i in 1..20
   puts i
 end
 
+
 # 標準出力
 n = gets.chomp
 
 for i in 0..n
   puts "おはよう"
 end
+
+
+# 標準出力 num1~num2
+
+num1 = gets.to_i
+num2 = gets.to_i
+
+for i in num1..num2
+  puts i 
+end
+
+
+#標準出力 count数 x 入力文字
+
+count = gets.to_i
+
+for i in 1..count
+  msg = gets.chomp
+  puts msg
+end
+
