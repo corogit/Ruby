1515a86a363aedca862f017ae3c83079eeb90e3b
corogit <66649016+corogit@users.noreply.github.com>
2021-03-19 12:21:03 +0900

    [シンボル]

---
+++ ruby-book/lib/5.3.1.rb
@@ -0,0 +1,12 @@
+# symbol
+
+:apple #symbol
+'apple' #string
+
+:apple.object_id #=>11111
+:apple.object_id #=>11111
+:apple.object_id #=>11111
+
+'apple'.object_id #=>11111
+'apple'.object_id #=>11112
+'apple'.object_id #=>11113
